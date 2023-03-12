c1c7c3890c74886b4a7e8fc997d7a366299a4984
Samuel Bosawer <zam.bosawer@gmail.com>
2023-03-12 22:32:45 +0900

    Undangan Azarya dan maya

---
+++ azarya-maya/js/nama-tamu.js
@@ -0,0 +1,11 @@
+    // Nama Undangan
+    let url = window.location.href;
+    let cut = url.substr(50);
+
+    if(cut==="")
+    {
+        document.getElementById("nama").innerHTML = `Belum ada nama tamu undangan`; 
+    }else{
+        let nama = cut.replace(/-/g, " ");
+        document.getElementById("nama").innerHTML = nama;   
+    }
---
+++ azarya-maya/js/udangan.js
@@ -0,0 +1,35 @@
+const copyBtn = document.getElementById('btn-undangan')
+const copyText = document.getElementById('tamu')
+
+copyBtn.onclick = () => {
+    copyText.select();
+    let teks = copyText.value;
+    if(teks === '')
+    {
+        Swal.fire({         //displays a pop up with sweetalert
+        icon: 'error',
+        text: 'Masukan nama tamu dengan benar',
+        showConfirmButton: false,
+        timer: 3000
+    });
+    }else{
+        let nama = teks.replace(/ /g,"-");
+        let base_url = window.location.href;
+        let url = `Shalom jangan lupa datang dalam acara pernikahan kami 😇
+        
+http://127.0.0.1:5500/azarya-maya/index.html?nama=${nama}
+
+Azarya & Maya
+Terimakasih 😁`;
+        navigator.clipboard.writeText(url);
+        // let url =
+        document.execCommand('copy');    // Simply copies the selected text to clipboard 
+        Swal.fire({         //displays a pop up with sweetalert
+            icon: 'success',
+            text: 'Link sudah tersalin !!',
+            showConfirmButton: false,
+            timer: 3000
+        });
+    }
+   
+}
