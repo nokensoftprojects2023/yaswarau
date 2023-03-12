74aaac49e371a80d8040d5715354405980f9d029
Samuel Bosawer <zam.bosawer@gmail.com>
2023-03-12 23:27:26 +0900

    Undangan Azarya dan maya #9

--- azarya-maya/js/nama-tamu-dua.js
+++ azarya-maya/js/nama-tamu-dua.js
@@ -1,6 +1,7 @@
     // Nama Undangan
+    // https://yaswarau.id/azarya-maya/index.html?nama=samuel+bosawr
     let url = window.location.href;
-    let cut = url.substr(41);
+    let cut = url.substr(47);
     if(cut==="")
     {
         document.getElementById("nama").innerHTML = `Belum ada nama tamu undangan`; 
